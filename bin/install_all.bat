c0e2ba949cf1f602c7f95f897143cf0e121afe72
AnastasiaShemyakinskaya <anastasia.shemyakins@dell.com>
2020-11-24 14:07:57 +0300

    Fix scripts

--- bin/install_all.bat
+++ bin/install_all.bat
@@ -6,7 +6,7 @@ SET DIR=%~dp0%
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "[System.Net.ServicePointManager]::SecurityProtocol = 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 
 ::run docker installer
-%systemroot%\System32\WindowsPowerShell\v1.0\powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "& '%DIR%script\install_docker.ps1' %*"
+%systemroot%\System32\WindowsPowerShell\v1.0\powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "& '%DIR%scripts\install_docker.ps1' %*"
 
 ::setup elastic and kibana
-%systemroot%\System32\WindowsPowerShell\v1.0\powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "& '%DIR%script\setup_docker.ps1' %*"
+%systemroot%\System32\WindowsPowerShell\v1.0\powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "& '%DIR%scripts\setup_docker.ps1' %*"
