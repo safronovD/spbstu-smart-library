be78abbd6ec6c07d651ff571e5c55fce63c755ba
safronovD <danil_safronov@dell.com>
2021-05-04 11:18:39 +0300

    saved

--- connector/pkg/persistent/fs_writer_test.go
+++ connector/pkg/persistent/fs_writer_test.go
@@ -2,13 +2,15 @@ package persistent
 
 import (
 	"encoding/json"
-	"github.com/stretchr/testify/assert"
 	"io/ioutil"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 const (
 	json_path = "/tmp/test.json"
+	json_dir  = "/tmp/json"
 )
 
 type TestStruct struct {
@@ -19,13 +21,8 @@ type TestStruct struct {
 }
 
 func TestSaveJSON(t *testing.T) {
-	// Create Config struct and set some fields
-	testStruct := &TestStruct{}
-
-	testStruct.Field1 = "string"
-	testStruct.Field2 = 23
-	testStruct.Field3 = true
-	testStruct.Field4 = []string{"str1", "str2"}
+	// Create Test struct and set some fields
+	testStruct := createTestStruct()
 
 	// Write data to file
 	jsonData, err := json.Marshal(testStruct)
@@ -34,12 +31,38 @@ func TestSaveJSON(t *testing.T) {
 	err = SaveJSON(jsonData, json_path)
 	assert.Nil(t, err)
 
-	// Read data from file with NewConfig method and check that structures are the same
-	readData, err := ioutil.ReadFile(json_path)
-	assert.Nil(t, err)
-
-	readTestStruct := &TestStruct{}
-	err = json.Unmarshal(readData, readTestStruct)
+	// Read data from file and check that structures are the same
+	readTestStruct, err := readTestStruct(json_path)
 	assert.Nil(t, err)
 	assert.Equal(t, testStruct, readTestStruct)
+}
+
+func TestNewFileSystemWriter(t *testing.T) {
+	fsw1 := NewFileSystemWriter(json_dir)
+	fsw2 := NewFileSystemWriter(json_dir)
+
+	assert.Equal(t, fsw1, fsw2)
+}
+
+func createTestStruct() *TestStruct {
+	return &TestStruct{
+		Field1: "string",
+		Field2: 23,
+		Field3: true,
+		Field4: []string{"str1", "str2"},
+	}
+}
+
+func readTestStruct(path string) (*TestStruct, error) {
+	readData, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	readTestStruct := &TestStruct{}
+	if err = json.Unmarshal(readData, readTestStruct); err != nil {
+		return nil, err
+	}
+
+	return readTestStruct, nil
 }
